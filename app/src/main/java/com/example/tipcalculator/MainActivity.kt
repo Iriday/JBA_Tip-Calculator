5ff4ba6ab17d8c566c2d4050a7918a25bb9fcdbd
Igor <GI_Something@protonmail.com>
2021-03-03 09:01:36 +0200

    feat: Impl listeners

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val billEditText = findViewById<EditText>(R.id.edit_text)
+        val tipPercentSlider = findViewById<Slider>(R.id.slider)
+        val tipTextView = findViewById<TextView>(R.id.text_view)
+
+        billEditText.addTextChangedListener {
+            tipTextView.text =
+                if (it.isNullOrBlank())
+                    ""
+                else
+                    toResultStr(it.toString(), tipPercentSlider.value)
+        }
+
+        tipPercentSlider.addOnChangeListener { _, value, _ ->
+            tipTextView.text =
+                if (billEditText.text.isNullOrBlank())
+                    ""
+                else
+                    toResultStr(billEditText.text.toString(), value)
+        }
+
+    }
+
+    private fun toResultStr(bill: String, percentage: Float): String {
+        return "Bill value: $bill, tip percentage: $percentage%"
     }
 }
